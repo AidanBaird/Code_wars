37232d724f81b2ae8698a7f226a182cba3848ac0
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-05-10 19:15:33 +0100

    Add files via upload

---
+++ Students_final_grade_kyu8.py
@@ -0,0 +1,11 @@
+# A function that grades a persons final result by taking into account their exam grade and the amount of projects they've finished
+
+def final_grade(exam, projects):
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0
