8c8d470f10eb2a4db479aa298a5556c06a2aadb6
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-06-08 22:04:41 +0100

    Update Friend_or_foe_kyu7.py

--- Friend_or_foe_kyu7.py
+++ Friend_or_foe_kyu7.py
@@ -1,4 +1,4 @@
-# People would four letter names are your only friends and this function checks if they are friendly or not
+# People with four letter names are your only friends and this function checks if they are friendly or not
 
 def friend(x):
     friend = []
