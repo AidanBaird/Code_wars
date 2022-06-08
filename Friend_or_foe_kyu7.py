91aafc9f86dddc27350571c8dfd72e12dc5aa30f
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-06-08 21:44:17 +0100

    Add files via upload

---
+++ Friend_or_foe_kyu7.py
@@ -0,0 +1,8 @@
+# People would four letter names are your only friends and this function checks if they are friendly or not
+
+def friend(x):
+    friend = []
+    for person in x:
+        if len(person) == 4:
+            friend.append(person)
+    return friend
