02c350111b9ff045f962e6507ebb085cb438a3aa
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-06-21 23:56:28 +0100

    Update 5_without_numbers_kyu8.py

--- 5_without_numbers_kyu8.py
+++ 5_without_numbers_kyu8.py
@@ -1,4 +1,5 @@
-# Create a function that returns 5 as an int without using the characters 0123456789*+-/
+# # Create a function that returns 5 as an int without using the characters 0123456789*+-/
+
 
 def unusual_five():
     x = "fives"
