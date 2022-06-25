0a498bb2d2079f2aacb9fd12af59a32be65c9b17
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-06-25 23:14:38 +0100

    Add files via upload

---
+++ Convert_a_Bolean_to_a_string_kyu8.py
@@ -0,0 +1,7 @@
+# check if b is either True or False and return a string reponse instead
+
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    elif b == False:
+        return "False"
