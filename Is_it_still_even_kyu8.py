48afb1ce0a6bc67297b760b008cf84ced8f35666
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-08-21 23:59:42 +0100

    Add files via upload

---
+++ Is_it_still_even_kyu8.py
@@ -0,0 +1,8 @@
+# Find ifa number is even
+# if statement that checks if there is any remainder of two and if not it's even
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
