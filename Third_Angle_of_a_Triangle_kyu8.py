1f091380cb763e2c712d0234df99d4e491fe030a
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-08-27 00:39:40 +0100

    Add files via upload

---
+++ Third_Angle_of_a_Triangle_kyu8.py
@@ -0,0 +1,5 @@
+# Finding the angle of the last corner in a triangle given two of the angles
+# Set the angle of a triangle and take away the two given angles
+
+def other_angle(a, b):
+    return 180 - (a + b)
